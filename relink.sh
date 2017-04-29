98feee0451826a3cb19553a3155f617582759148
educatedCaveman <j.drake.hoffman@gmail.com>
2017-04-29 14:44:07 -0400

    basic linking script

---
+++ relink.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#TODO:
+#generalize things/paths
+
+ln -s /home/drake/dotfiles/init.vim /home/drake/.config/nvim/init.vim
+ln -s /home/drake/dotfiles/init.vim /home/drake/.nvimrc
+ln -s /home/drake/dotfiles/vimrc /home/drake/.vimrc
+ln -s /home/drake/dotfiles/i3config.NeonBlack /home/drake/.config/i3/config
+ln -s /home/drake/dotfiles/py3status.conf.NeonBlack /home/drake/.py3status.conf
+ln -s /home/drake/dotfiles/i3status.conf /home/drake/.i3status.conf
+ln -s /home/drake/dotfiles/bash_aliases.NeonBlack /home/drake/.bash_aliases
+ln -s /home/drake/dotfiles/bashrc.NeonBlack /home/drake/.bashrc
+ln -s /home/drake/dotfiles/terminator.config /home/drake/.config/terminator/config
