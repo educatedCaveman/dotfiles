0989dfad3105615631431db8c40403723750e521
Drake <j.drake.hoffman@gmail.com>
2019-04-20 12:45:18 -0400

    added redshift, and generalized home directory

--- access-terminal/links.sh
+++ access-terminal/links.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-ln -s /home/drake/dotfiles/access-terminal/terminator.config /home/drake/.config/terminator/config
-ln -s /home/drake/dotfiles/access-terminal/bash_aliases /home/drake/.bash_aliases
-ln -s /home/drake/dotfiles/access-terminal/bashrc /home/drake/.bashrc
-ln -s /home/drake/dotfiles/init.vim /home/drake/.config/nvim/init.vim
-ln -s /home/drake/dotfiles/init.vim /home/drake/.nvimrc
-ln -s /home/drake/dotfiles/vimrc /home/drake/.vimrc
-ln -s /home/drake/dotfiles/access-terminal/i3config /home/drake/.config/i3/config
-ln -s /home/drake/dotfiles/access-terminal/py3status.conf /home/drake/.py3status.conf
-
+ln -s $HOME/dotfiles/access-terminal/terminator.config $HOME/.config/terminator/config
+ln -s $HOME/dotfiles/access-terminal/bash_aliases $HOME/.bash_aliases
+ln -s $HOME/dotfiles/access-terminal/bashrc $HOME/.bashrc
+ln -s $HOME/dotfiles/init.vim $HOME/.config/nvim/init.vim
+ln -s $HOME/dotfiles/init.vim $HOME/.nvimrc
+ln -s $HOME/dotfiles/vimrc $HOME/.vimrc
+ln -s $HOME/dotfiles/access-terminal/i3config $HOME/.config/i3/config
+ln -s $HOME/dotfiles/access-terminal/py3status.conf $HOME/.py3status.conf
+ln -s $HOME/dotfiles/redshift.conf $HOME/.config/redshift.conf
