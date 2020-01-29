5e3069e87dda693af1da637e76cf4361e126d0ae
drake <j.drake.hoffman@gmail.com>
2020-01-29 06:53:31 -0500

    systemd vars for ff wayland

--- access-terminal/links.sh
+++ access-terminal/links.sh
@@ -9,3 +9,5 @@ ln -s $HOME/dotfiles/vimrc $HOME/.vimrc
 ln -s $HOME/dotfiles/access-terminal/i3config $HOME/.config/i3/config
 ln -s $HOME/dotfiles/access-terminal/py3status.conf $HOME/.py3status.conf
 ln -s $HOME/dotfiles/redshift.conf $HOME/.config/redshift.conf
+#for firefox wayland
+ln -s $HOME/dotfile/envvars.conf $HOME/.config/environment.d/envvars.conf
