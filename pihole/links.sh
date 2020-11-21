72bd5da2ed461e95a36a89964681299d3ddcc3eb
Drake <j.drake.hoffman@gmail.com>
2020-11-21 11:26:26 -0500

    add pihole todos

--- pihole/links.sh
+++ pihole/links.sh
@@ -5,6 +5,8 @@ PIHOLE="/etc/pihole"
 #WEB="/var/www/html/admin/style/vendor"
 DOTFILES="${HOME}/dotfiles/pihole"
 
+#TODO: link regex, FTL
+
 #first copy the default files that we'll be linking:
 mkdir "${BACKUP}" 
 cp "${HOME}"/.bash_aliases "${BACKUP}"/bash_aliases
@@ -35,9 +37,12 @@ sudo ln -s "${DOTFILES}"/bashrc "${HOME}"/.bashrc
 sudo ln -s "${HOME}"/dotfiles/vimrc "${HOME}"/.vimrc
 sudo ln -s "${HOME}"/dotfiles/init.vim "${HOME}"/.config/nvim/init.vim
 sudo ln -s "${DOTFILES}"/tmux.conf "${HOME}"/.tmux.conf
+#TODO: add logic to pick the correct host file.  maybe be interactive?
 sudo ln -s "${DOTFILES}"/hosts.file /etc/hosts
 sudo ln -s "${DOTFILES}"/whitelist.txt "${PIHOLE}"/whitelist.txt
 sudo ln -s "${DOTFILES}"/blacklist.txt "${PIHOLE}"/blacklist.txt
 sudo ln -s "${DOTFILES}"/adlists.list "${PIHOLE}"/adlists.list
+
+#TODO: run zsh config, too
 
 echo "now look here:  https://github.com/jacobbates/pi-hole-midnight"
