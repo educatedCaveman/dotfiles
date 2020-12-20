bf1555596d82ee885586f8f8377529705143a260
Drake <j.drake.hoffman@gmail.com>
2020-12-20 13:06:18 -0500

    trim unused files

--- holodeck/links.sh
+++
@@ -1,6 +0,0 @@
-ln -s /Users/drake/dotfiles/vinculum/bash_aliases /Users/drake/.bash_aliases
-ln -s /Users/drake/dotfiles/vinculum/bash_profile /Users/drake/.bash_profile
-ln -s /Users/drake/dotfiles/vinculum/bashrc /Users/drake/.bashrc
-ln -s /Users/drake/dotfiles/vinculum/profile /Users/drake/.profile
-ln -s /Users/drake/dotfiles/vinculum/tmux.conf /Users/drake/.tmux.conf
-
